8b39ffe7824ba04fecc7f31d188a8697dc33990e
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-17 23:50:05 +0430

    geting data from input

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
