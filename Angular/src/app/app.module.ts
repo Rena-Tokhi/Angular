10d70f9eccb4bb5bdff07ccc5db54e6b5cb4a7f0
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-18 00:52:40 +0430

    srever component added

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent,
     
   ],
   imports: [
