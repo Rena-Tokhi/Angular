0d69f7c24e7b1870a858e443e805e714eae39e1a
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-18 20:56:12 +0430

    inline template

--- Angular/src/app/server/server.component.ts
+++ Angular/src/app/server/server.component.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html',
+  template: `
+  <app-servers></app-servers>
+  <p> do not use this way (inline template )if you have more then 3 lines of code </p>
+  `,
   styleUrls: ['./server.component.css']
 })
 export class ServerComponent implements OnInit {
