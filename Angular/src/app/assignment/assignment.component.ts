6fb9abf9646c5ff4272f3a6cd82101f628173c9f
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-20 18:02:54 +0430

    toggle btn

--- Angular/src/app/assignment/assignment.component.ts
+++ Angular/src/app/assignment/assignment.component.ts
@@ -6,10 +6,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./assignment.component.css']
 })
 export class AssignmentComponent implements OnInit {
-
+ showP1 = false;
+ showP = false;
+   log : number[] = [];
+ 
   constructor() { }
 
   ngOnInit(): void {
+  }
+    onToggle1(){
+    this.showP1 = !this.showP1;
+  }
+  onToggle(){
+    this.showP = !this.showP;
+    this.log.push(this.log.length + 1);
+
   }
 
 }
