c2125654d94f7d60c1ddba84617bd016fa96873e
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-23 09:57:28 +0430

    bug fixed

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,5 +1,6 @@
 
 import { Component } from '@angular/core';   
+import * as CryptoJS from 'crypto-js';  
 
 @Component({  
   selector: 'app-root',  
@@ -9,10 +10,18 @@ import { Component } from '@angular/core';
 export class AppComponent {  
 
    title = 'Angular';
-  name = "Tokhi"
+  name = "Tokhi";
+  enc : any  = '';
+
+  
 
 
   constructor() {  
+    var hash = CryptoJS.MD5("Message");
+    console.warn(typeof hash);
+
+    var hash = CryptoJS.SHA256("Message"); alert(typeof hash); // object alert(hash); 
+    
   }  
    
 }
