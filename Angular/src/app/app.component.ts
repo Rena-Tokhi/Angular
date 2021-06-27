09c7bc3807c6e72f8d47caf7d2f467d0677e56fa
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-27 12:06:39 +0430

    bug fixed

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -12,21 +12,11 @@ export class AppComponent {
 
   title = 'Angular';
   name = "Tokhi";
-  enc: any = '';
-  server: any [] = [];
+
   constructor() {
-
-    var hash = CryptoJS.MD5("Message");
-    console.warn(typeof hash);
-    var hash = CryptoJS.SHA256("Message");// object alert(hash); 
+   
 
   }
-  onAddServer() {
-    this.server.push('Another Server')
-  }
-  onRemovServer(id :number) {
-  const position = id + 1;
-  this.server.splice(position ,1 )
-  }
+
 
 }
