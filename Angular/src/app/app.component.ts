32c0d46233fff8cb8d3550576617f0d2e04198db
Rina Tokhi <Rina.Tokhi7@gmail.com>
2021-06-28 21:19:42 +0430

    spiliting Apps to components

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -13,24 +13,8 @@ export class AppComponent {
   title = 'Angular';
   name = "Tokhi";
   serverElements :any[] = [];
-  newServerName = '';
-  newServerContent = '';
+ 
 
-  onAddServer() {
-    this.serverElements.push({
-      type: 'server',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
-
-  onAddBlueprint() {
-    this.serverElements.push({
-      type: 'blueprint',
-      name: this.newServerName,
-      content: this.newServerContent
-    });
-  }
 
 
 }
